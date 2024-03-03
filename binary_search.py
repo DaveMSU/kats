10c5313424932f3b424f83bbf57963af772c113e
DaveMSU <davehum@mail.ru>
2024-03-03 14:54:46 +0300

    Binary search function has been added.

---
+++ binary_search.py
@@ -0,0 +1,13 @@
+import typing as tp
+from functools import total_ordering
+
+
+def binary_search(arr: tp.List[total_ordering], x: total_ordering) -> bool:
+    l, m, r = 0, len(arr) // 2, len(arr) - 1
+    while l <= r:
+        if arr[m] == x:
+            return m
+        else:
+            l, r = (m + 1, r) if arr[m] < x else (l, m - 1)
+            m = l // 2 + r // 2 + (l % 2) * (r % 2)
+    return m + 1
