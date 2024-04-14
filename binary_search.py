1332d1eef985cff1469494a9f1362974dcada3d2
DaveMSU <davehum@mail.ru>
2024-04-14 16:24:48 +0300

    Remove extra changes.

--- binary_search.py
+++ binary_search.py
@@ -2,7 +2,7 @@ import typing as tp
 from functools import total_ordering
 
 
-def binary_search(arr: tp.List[total_ordering], x: total_ordering) -> int:
+def binary_search(arr: tp.List[total_ordering], x: total_ordering) -> bool:
     l, m, r = 0, len(arr) // 2, len(arr) - 1
     while l <= r:
         if arr[m] == x:
@@ -10,4 +10,4 @@ def binary_search(arr: tp.List[total_ordering], x: total_ordering) -> int:
         else:
             l, r = (m + 1, r) if arr[m] < x else (l, m - 1)
             m = l // 2 + r // 2 + (l % 2) * (r % 2)
-    return l
+    return m + 1
